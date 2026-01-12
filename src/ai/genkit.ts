814b623633678eee8dff0135d0fbfc2abe56ed30
Nico Chepo <nicochepo@gmail.com>
2026-01-12 18:16:28 +0000

    aun me sale el mensje , podria ser otra cosa

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,15 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
-  model: 'googleai/gemini-2.5-flash',
+  plugins: [
+    googleAI({
+      // Supported models: https://ai.google.dev/gemini-api/docs/models/gemini
+      // We recommend using a `latest` model as that will automatically be
+      // updated to point to the latest stable version.
+      model: 'gemini-1.5-flash-latest',
+      output: {
+        format: 'json',
+      },
+    }),
+  ],
 });
